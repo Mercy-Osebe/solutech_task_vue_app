81d5bd0eb93bc5f686d75f44db07d0377dd3cea2
engmercy <marcynyaata@gmail.com>
2023-04-20 15:02:56 +0300

    added navigation guards to protect our auth routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,64 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import TaskIndex from '../views/task/TaskIndex.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import TaskIndex from "../views/task/TaskIndex.vue";
+import { useAuthUser } from "../stores/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/taskIndex',
-      name: 'taskIndex',
-      component: TaskIndex
+      path: "/taskIndex",
+      name: "taskIndex",
+      component: TaskIndex,
+      meta: { requiresAuth: true },
     },
     {
-      path: '/createTask',
-      name: 'createTask',
-      component: () => import('../views/task/CreateTask.vue')
+      path: "/createTask",
+      name: "createTask",
+      component: () => import("../views/task/CreateTask.vue"),
+      meta: { requiresAuth: true },
     },
     {
-      path: '/showTask/:id',
-      name: 'showTask',
-      component: () => import('../views/task/ShowTask.vue')
+      path: "/showTask/:id",
+      name: "showTask",
+      component: () => import("../views/task/ShowTask.vue"),
+      meta: { requiresAuth: true },
     },
     {
-      path: '/editTask/:id/edit',
-      name: 'editTask',
-      component: () => import('../views/task/EditTask.vue')
+      path: "/editTask/:id/edit",
+      name: "editTask",
+      component: () => import("../views/task/EditTask.vue"),
+      meta: { requiresAuth: true },
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/auth/Login.vue')
+      path: "/login",
+      name: "login",
+      component: () => import("../views/auth/Login.vue"),
+      meta: { requiresUnauth: true },
     },
     {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/auth/Register.vue')
+      path: "/register",
+      name: "register",
+      component: () => import("../views/auth/Register.vue"),
+      meta: { requiresUnauth: true },
     },
-  ]
-})
+  ],
+});
+router.beforeEach(function (to, _, next) {
+  if (to.meta.requiresAuth && !useAuthUser.token) {
+    next('/login')
+  }
+  else if (to.meta.requiresUnauth && useAuthUser.token) {
+    next('/taskIndex')
+  }
+  else{
+    next()
+  }
+});
 
-export default router
+export default router;
